a1f29399c7e8d8c934ea583c30504a2a8186c79e
Alexander Hering <dev.hering@gmail.com>
2023-05-22 20:31:35 +0200

    fixed python call

--- run_privateGPT.sh
+++ run_privateGPT.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-source /privateGPT-container/venv/bin/activate && cd /privateGPT-container/privateGPT/ && python privateGPT.py
+source /privateGPT-container/venv/bin/activate && cd /privateGPT-container/privateGPT/ && python3 privateGPT.py
