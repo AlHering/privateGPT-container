98cdd32abe93e0e43b518fe1489464ef0f355cfd
Alexander Hering <dev.hering@gmail.com>
2023-05-22 22:39:59 +0200

    extended runner script

--- run_privateGPT.sh
+++ run_privateGPT.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-source /privateGPT-container/venv/bin/activate && cd /privateGPT-container/privateGPT/ && python3 privateGPT.py
+source /privateGPT-container/venv/bin/activate && python3 set_model.py && cd /privateGPT-container/privateGPT/ && python3 ingest.py && python3 privateGPT.py
